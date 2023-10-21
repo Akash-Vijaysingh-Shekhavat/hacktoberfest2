98fff1715861bf348abfc4e36fff61d49bb68ec9
Akash Vijaysingh Shekhavat <mycountryakash@gmail.com>
2023-10-21 07:43:34 +0530

    The most asked SQL querries in Product-Based-Companies with all types of possible solution

---
+++ SQL/Q1_Employees Earning More Than Their Managers.sql
@@ -0,0 +1,30 @@
+/*Employees Earning More Than Their Managers
+Write a solution to find the employees who earn more than their managers.
+Return the result table in any order.
+Link:https://leetcode.com/problems/employees-earning-more-than-their-managers/
+The solution to problem is given solution 4 is the most optimzed solution
+ */
+/* Solution 1 */
+SELECT name AS Employee FROM Employee
+WHERE Employee.Salary >(SELECT salary FROM
+    Employee AS E2 WHERE Id = Employee.ManagerId);
+
+/* Solution 2 */
+SELECT e1.name AS Employee # Step 3 : Renaming the name as Employee
+FROM Employee e1
+         LEFT JOIN Employee e2    # Step 1: Using the left join
+ON e1.managerId = e2.id
+WHERE e1.salary >e2.salary  ;# Step 2: Checking condition
+/* Solution 3 */
+SELECT e1.name AS Employee # Step 3 : Renaming the name as Employee
+FROM Employee e1
+         INNER JOIN Employee e2    # Step 1: Using the left join
+ON e1.managerId = e2.id
+WHERE e1.salary >e2.salary  ;# Step 2: Checking condition
+
+/* Solution 4 */
+SELECT e1.name AS Employee # Step 3 : Renaming the name as Employee
+FROM Employee e1
+         INNER JOIN Employee e2    # Step 1: Using the left join
+ON e1.managerId = e2.id
+WHERE e1.salary >e2.salary  ;# Step 2: Checking condition
